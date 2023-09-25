3faee80adfa29a7a3c780a32fed127983944a383
Nicolas <contact@nicolasrichelet.com>
2023-09-25 22:15:10 +0200

    mise en place des boutons catégories et filtres

--- FrontEnd/scripts/works.js
+++ FrontEnd/scripts/works.js
@@ -1,7 +1,19 @@
 // Récupération des projets du site depuis l'API
-
 const works = await fetch("http://localhost:5678/api/works").then(works => works.json())
 const categories = await fetch("http://localhost:5678/api/categories").then(categories => categories.json())
+
+const worksSet = new Set()
+const categoriesSet = new Set()
+worksSet.add (works)
+categoriesSet.add(categoriesSet)
+
+for (let item of worksSet) {
+    for (let object of item) {
+    console.log (object.title)}
+}
+
+let worksFiltered = works // Liste des projets après filtres, complète par défaut
+
 
 function showWorks(worksFiltered) {    
     // Effacer l'ancienne gallerie
@@ -16,7 +28,7 @@ function showWorks(worksFiltered) {
         imageElement.src = worksFiltered[i].imageUrl // ajout de l'url de l'image
         imageElement.alt = worksFiltered[i].title // ajout de la balise Alt
         const figcaptionTag = document.createElement('figcaption')
-        figcaptionTag.innerText = works[i].title
+        figcaptionTag.innerText = worksFiltered[i].title
         
         // Affichage elements
         figureTag.appendChild(imageElement)
@@ -25,24 +37,40 @@ function showWorks(worksFiltered) {
     }
 }
 
+{
 //afficher les boutons de categorie
 const filtresGallery = document.querySelector(".filtres ul")
 const listeButton = document.createElement('li')
+    // Bouton catégorie "Tous"
 const buttonCatTous = document.createElement('button')
-buttonCatTous.dataset.cat = 'tous'
-buttonCatTous.innerText = 'Tous'
-
-// Bouton Tous
+buttonCatTous.dataset.id = '0'
+buttonCatTous.innerText = "Tous"
+buttonCatTous.classList.add('button-categorie')
 listeButton.appendChild(buttonCatTous)
 filtresGallery.appendChild(listeButton)
-
-// Autres bouton categories
+    // Autres bouton categories
 for (let i=0; i <categories.length; i++) {
     const buttonCat = document.createElement('button')
-    buttonCat.dataset.id = i
+    buttonCat.classList.add('button-categorie')
+    buttonCat.dataset.id = i+1
     buttonCat.innerText = categories[i].name
     listeButton.appendChild(buttonCat) 
     filtresGallery.appendChild(listeButton)
 }
+}
 
-showWorks(works)
+// function filterByCategorie (worksSet, categoriesSet) {
+const filterCategoryButton = document.querySelectorAll(".button-categorie")
+for (let i=0; i <filterCategoryButton.length; i++) {
+    filterCategoryButton[i].addEventListener('click', (event) => {
+        console.log(parseInt(event.target.dataset.id))
+        if (parseInt(event.target.dataset.id) !== 0) {
+            worksFiltered = works.filter((item) => item.categoryId === parseInt(event.target.dataset.id))
+            showWorks(worksFiltered)
+        } else {
+            showWorks(works)
+        }
+    })
+}
+
+showWorks(worksFiltered)
