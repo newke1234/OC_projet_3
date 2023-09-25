d3f2f583e097aca2706a23f54959c2ca16e90f8d
Nicolas <contact@nicolasrichelet.com>
2023-09-26 00:09:40 +0200

    changé les ' vers " - corrigé balises li manquantes

--- FrontEnd/scripts/works.js
+++ FrontEnd/scripts/works.js
@@ -1,4 +1,4 @@
-// Récupération des projets du site depuis l'API
+// Récupération des projets du site depuis l"API
 const works = await fetch("http://localhost:5678/api/works").then(works => works.json())
 const categories = await fetch("http://localhost:5678/api/categories").then(categories => categories.json())
 
@@ -6,18 +6,18 @@ const categoriesSet = new Set(categories) // Set pour éviter les doublons
 let worksFiltered = works // Tableau des projets filtrée, ici : liste complète par défaut
 
 function showWorks(worksFiltered) {    
-    // Effacer l'ancienne gallerie
-    const galleryContent = document.querySelector('.gallery')
+    // Effacer l"ancienne gallerie
+    const galleryContent = document.querySelector(".gallery")
     galleryContent.innerHTML = ""
 
     // Affichage de la gallerie 
     for (let i=0; i < worksFiltered.length; i++) {
         // Reconstruction des balises
-        const figureTag = document.createElement('figure')
-        const imageElement = document.createElement('img')
-        imageElement.src = worksFiltered[i].imageUrl // ajout de l'url de l'image
+        const figureTag = document.createElement("figure")
+        const imageElement = document.createElement("img")
+        imageElement.src = worksFiltered[i].imageUrl // ajout de l"url de l"image
         imageElement.alt = worksFiltered[i].title // ajout de la balise Alt
-        const figcaptionTag = document.createElement('figcaption')
+        const figcaptionTag = document.createElement("figcaption")
         figcaptionTag.innerText = worksFiltered[i].title
         
         // Affichage elements
@@ -29,29 +29,30 @@ function showWorks(worksFiltered) {
 
 { //afficher les boutons de categorie
 const filtresGallery = document.querySelector(".filtres ul")
-const listeButton = document.createElement('li')
+const listeButton = document.createElement("li")
     // Bouton catégorie "Tous"
-const buttonCatTous = document.createElement('button')
-buttonCatTous.dataset.id = '0'
+const buttonCatTous = document.createElement("button")
+buttonCatTous.dataset.id = "0"
 buttonCatTous.innerText = "Tous"
-buttonCatTous.classList.add('button-categorie')
+buttonCatTous.classList.add("button-categorie")
 listeButton.appendChild(buttonCatTous)
 filtresGallery.appendChild(listeButton)
     // Autres bouton categories
 // for (let i=0; i <categoriesSet.length; i++) {
 
 categoriesSet.forEach( item => {
-    const buttonCat = document.createElement('button')
-    buttonCat.classList.add('button-categorie')
+    const buttonCat = document.createElement("button")
+    buttonCat.classList.add("button-categorie")
     buttonCat.dataset.id = item.id
     buttonCat.innerText = item.name
+    const listeButton = document.createElement("li")
     listeButton.appendChild(buttonCat) 
     filtresGallery.appendChild(listeButton)
 })
 
 // for (let item of categoriesSet) {
-//     const buttonCat = document.createElement('button')
-//     buttonCat.classList.add('button-categorie')
+//     const buttonCat = document.createElement("button")
+//     buttonCat.classList.add("button-categorie")
 //     buttonCat.dataset.id = item.id
 //     buttonCat.innerText = item.name
 //     listeButton.appendChild(buttonCat) 
@@ -62,7 +63,7 @@ categoriesSet.forEach( item => {
 // function filterByCategorie (worksSet, categoriesSetSet) {
 const filterCategoryButton = document.querySelectorAll(".button-categorie")
 for (let i=0; i <filterCategoryButton.length; i++) {
-    filterCategoryButton[i].addEventListener('click', (event) => {
+    filterCategoryButton[i].addEventListener("click", (event) => {
         if (parseInt(event.target.dataset.id) !== 0) {
             worksFiltered = works.filter((item) => item.categoryId === parseInt(event.target.dataset.id)) // Methode Avec filter() //
             showWorks(worksFiltered)
