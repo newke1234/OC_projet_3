0eabcd6f5fc4d1e38b6293bd1fb5a18ed95b3496
Nicolas <contact@nicolasrichelet.com>
2023-09-25 16:11:09 +0200

    creation des boutons filtres par catégories

--- FrontEnd/scripts/works.js
+++ FrontEnd/scripts/works.js
@@ -3,19 +3,18 @@
 const works = await fetch("http://localhost:5678/api/works").then(works => works.json())
 const categories = await fetch("http://localhost:5678/api/categories").then(categories => categories.json())
 
-
-function showWorks(works) {    
+function showWorks(worksFiltered) {    
     // Effacer l'ancienne gallerie
     const galleryContent = document.querySelector('.gallery')
     galleryContent.innerHTML = ""
 
     // Affichage de la gallerie 
-    for (let i=0; i < works.length; i++) {
+    for (let i=0; i < worksFiltered.length; i++) {
         // Reconstruction des balises
         const figureTag = document.createElement('figure')
         const imageElement = document.createElement('img')
-        imageElement.src = works[i].imageUrl // ajout de l'url de l'image
-        imageElement.alt = works[i].title // ajout de la balise Alt
+        imageElement.src = worksFiltered[i].imageUrl // ajout de l'url de l'image
+        imageElement.alt = worksFiltered[i].title // ajout de la balise Alt
         const figcaptionTag = document.createElement('figcaption')
         figcaptionTag.innerText = works[i].title
         
@@ -27,7 +26,23 @@ function showWorks(works) {
 }
 
 //afficher les boutons de categorie
+const filtresGallery = document.querySelector(".filtres ul")
+const listeButton = document.createElement('li')
+const buttonCatTous = document.createElement('button')
+buttonCatTous.dataset.cat = 'tous'
+buttonCatTous.innerText = 'Tous'
 
+// Bouton Tous
+listeButton.appendChild(buttonCatTous)
+filtresGallery.appendChild(listeButton)
 
+// Autres bouton categories
+for (let i=0; i <categories.length; i++) {
+    const buttonCat = document.createElement('button')
+    buttonCat.dataset.id = i
+    buttonCat.innerText = categories[i].name
+    listeButton.appendChild(buttonCat) 
+    filtresGallery.appendChild(listeButton)
+}
 
 showWorks(works)
