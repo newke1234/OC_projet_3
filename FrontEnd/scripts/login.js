422ad380b35d1b65752cb17ecc0ab9b822860640
Nicolas <contact@nicolasrichelet.com>
2023-09-26 15:54:16 +0200

    correction name="password" et id ="password"  dans le form

---
+++ FrontEnd/scripts/login.js
@@ -0,0 +1,21 @@
+
+
+// on ecoute le bouton 'se connecter'
+const loginSubmit = document.getElementById("loginSubmit")
+loginSubmit.addEventListener('click', () => {
+
+    // on recuperer les données du formulaire
+    const inputEmail = document.getElementById("email")
+    const inputPassword = document.getElementById("pass")
+})
+
+// verifier que les champs ne sont pas vides
+// verifier format du mail et du mot de passe (avec regExp)
+// Envoyer requete poste avec les données
+// si reponse du serveur = 200, on connecte l'utilise
+    // on enregistre le token dans "sessionStorage"
+    // On retourne vers la page d'accueil
+// sinon, on met message non valide
+
+
+
