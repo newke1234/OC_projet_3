b44cf358dffc02ead12f899d4124737c1f908aa0
Nicolas <contact@nicolasrichelet.com>
2023-09-27 20:18:09 +0200

    Mise en place addEventlistener sur .popup-logout

--- FrontEnd/scripts/main.js
+++ FrontEnd/scripts/main.js
@@ -2,6 +2,7 @@
 
 // verifier si un utilisateur est loggué
 let tokenSession = JSON.parse(window.sessionStorage.getItem("token"))
+// tokenSession = ("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4") // je suis loggué
 if (tokenSession) {
     console.log(tokenSession)
     const menuLogout = document.querySelector(".menu-logout")
@@ -18,6 +19,7 @@ let categoriesSet = new Set(categories) // Set pour éviter les doublons
 let worksFiltered = works // Tableau des projets filtrée, ici : liste complète par défaut
 
 showFilterButtons(categoriesSet)
+showWorks(worksFiltered)
 
 // Listener pour les boutons categories 
 const filterCategoryButton = document.querySelectorAll(".button-categorie")
@@ -33,4 +35,22 @@ for (let i=0; i <filterCategoryButton.length; i++) {
     })
 }
 
-showWorks(worksFiltered)
+// Listener bouton logout
+const menuLogout = document.querySelector('.menu-logout')
+menuLogout.addEventListener('click', () => {
+    const popupLogout = document.querySelector('.popup-logout')
+    popupLogout.classList.remove('hidden')
+    const answerLogoutAnswerButtons = document.querySelectorAll('.popup-logout div button')
+    for (let i=0; i < answerLogoutAnswerButtons.length; i++) {
+        answerLogoutAnswerButtons[i].addEventListener('click', (event) => {
+            console.log(event)
+            if (event.target.dataset.answer === "yes") {
+                window.sessionStorage.removeItem("token")
+                window.location.href = "./index.html"
+            } else {
+                popupLogout.classList.add('hidden')
+                return
+            }
+        })
+    }
+})
