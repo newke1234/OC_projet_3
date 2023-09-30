93286193c40d5076abd074a7c41427a71c219b5b
Nicolas <contact@nicolasrichelet.com>
2023-09-30 23:42:11 +0200

    mise en place modale galerie photo #2

--- FrontEnd/scripts/main.js
+++ FrontEnd/scripts/main.js
@@ -7,11 +7,13 @@ let worksFiltered = works // Tableau des projets filtrée, ici : liste complète
 
 // verifier si un utilisateur est loggué
 let tokenSession = JSON.parse(window.sessionStorage.getItem("token"))
-tokenSession = ("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4") // je suis loggué
+// tokenSession = ("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4") // je suis loggué
 if (tokenSession) {
     const menuLogout = document.querySelector(".menu-logout")
     const menuLogin = document.querySelector(".menu-login")
     const modeEdition = document.querySelector(".modeEdition")
+    const modifierButton = document.querySelector(".button-modifier")
+    modifierButton.classList.remove("hidden")
     menuLogout.classList.remove("hidden")
     menuLogin.classList.add("hidden")
     modeEdition.classList.remove("hidden")
@@ -59,8 +61,36 @@ menuLogout.addEventListener('click', () => {
 })
 
 //Listener bouton modifier
-const modaleMain = document.querySelector(".button-modifier")
-modaleMain.addEventListener("click", () => {
-    
-} )
+const modalMain = document.querySelector(".modal-main")
+const modifierButton = document.querySelector(".button-modifier")
+modifierButton.addEventListener("click", async () => {
+    works = await fetch("http://localhost:" + apiPort + "/api/works").then(works => works.json())
+    const showGallery = document.querySelector(".showGallery")
+    showGallery.innerHTML = ""
+    // const modalMain = document.querySelector(".modal-main")
+    modalMain.classList.remove('hidden')
+    modalMain.setAttribute("aria-modal", "true")
+    document.querySelector('body').classList.add('no-scroll')
+    // Affichage de la gallerie 
+    for (let i=0; i < works.length; i++) {
+        const figureTag = document.createElement("figure")
+        const imageElement = document.createElement("img")
+        imageElement.src = works[i].imageUrl // ajout de l"url de l"image
+        imageElement.alt = works[i].title // ajout de la balise Alt
+        figureTag.appendChild(imageElement) // Affichage les nouveaux elements
+        showGallery.appendChild(figureTag)
+
+    }
+})
+
+//listener Croix de fermeture de la modale
+const crossIcon = document.querySelector(".fa-xmark")
+crossIcon.addEventListener('click', () => {
+    modalMain.classList.add('hidden')
+    modalMain.classList.remove('no-scroll')
+    document.querySelector('body').classList.remove('no-scroll')
+})
+
+// Listener hors modal
+const outOfModal = document
 
