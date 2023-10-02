821a4b46810b1a5233cc3a2f155a5f8dad489d97
Nicolas <contact@nicolasrichelet.com>
2023-10-02 22:50:19 +0200

    Simplification  de la fonction modalLogout

--- FrontEnd/scripts/modals.js
+++ FrontEnd/scripts/modals.js
@@ -1,22 +1,35 @@
+/**
+ * Fonction pour  gérer le bouton oui de la modale "logout"
+ * Le bouton "non" est géré avec la fonction "closeModal"
+ * @param {*} modalContent 
+ */
+function modalLogout(modalContent) {
+    // const answerLogoutButtons = document.querySelectorAll('#modal-logout div button')
+    // for (let i=0; i < answerLogoutButtons.length; i++) {
+    const answerLogoutButtons = document.getElementById("yes-button")
+    answerLogoutButtons.addEventListener('click', () => {
+        window.sessionStorage.removeItem("token")
+        window.location.href = "./index.html"
+    })
+}
 
 
+async function modalMain (modalContent) {
+    works = await fetch("http://localhost:" + apiPort + "/api/works").then(works => works.json())
+    const showGallery = document.querySelector(".showGallery")
+    showGallery.innerHTML = ""
+    // const modalMain = document.querySelector(".modal-main")
+    modalMain.classList.remove('hidden')
+    modalMain.setAttribute("aria-modal", "true")
+    document.querySelector('body').classList.add('no-scroll')
+    // Affichage de la gallerie 
+    for (let i=0; i < works.length; i++) {
+        const figureTag = document.createElement("figure")
+        const imageElement = document.createElement("img")
+        imageElement.src = works[i].imageUrl // ajout de l"url de l"image
+        imageElement.alt = works[i].title // ajout de la balise Alt
+        figureTag.appendChild(imageElement) // Affichage les nouveaux elements
+        showGallery.appendChild(figureTag)
 
-
-
-
-modalLogout = function () {
-    const answerLogoutButtons = document.querySelectorAll('.modal-logout div button')
-    for (let i=0; i < answerLogoutAnswerButtons.length; i++) {
-        answerLogoutButtons[i].addEventListener('click', (event) => {
-            popupLogout.setAttribute("aria-modal", "false")
-            if (event.target.dataset.answer === "yes") {
-                window.sessionStorage.removeItem("token")
-                window.location.href = "./index.html"
-            // } else {
-            //     popupLogout.classList.add('hidden')
-            //     document.querySelector('body').classList.remove('no-scroll')
-            //     return
-            }
-        })
     }
 }
