b9e2a679f8e981c718863262890f93c5963994c8
Nicolas <contact@nicolasrichelet.com>
2023-10-04 17:07:15 +0200

    Ajout function pour effecer un projet

--- FrontEnd/scripts/modals.js
+++ FrontEnd/scripts/modals.js
@@ -3,22 +3,21 @@
  * Le bouton "non" est géré avec la fonction "closeModal"
  */
 
+// const { validate } = require("json-schema")
+
 /**
  * Fonction Ouvrir une boite modale
  * @param {*} event 
  */
 function openModal (modal) {
-    // event.preventDefault()
-    // modal = document.querySelector(event.target.getAttribute('href'))
-    console.log(modal)
     modal.classList.remove("hidden")
     modal.setAttribute("aria-hidden", false)
     modal.setAttribute("aria-modal", true)
     document.querySelector('body').classList.add('no-scroll')
     if (modal.id === "modal-logout") modalLogout()
     if (modal.id === "modal-main")  modalBackOffice()
-    modal.addEventListener("click", () => closeModal(modal))
-    modal.querySelector(".js-modal-close").addEventListener("click", () => closeModal(modal))
+    if (modal.id !== "modal-logout-forced") modal.addEventListener("click", () => closeModal(modal))
+    if (modal.querySelector(".js-modal-close")) modal.querySelector(".js-modal-close").addEventListener("click", () => closeModal(modal))
     modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
 } 
 
@@ -29,7 +28,7 @@ function openModal (modal) {
  */
 function closeModal (modal) {
     if (modal === null) return
-    event.preventDefault()
+    // event.preventDefault()
     modal.classList.add("hidden")
     modal.setAttribute("aria-hidden", true)
     modal.setAttribute("aria-modal", false)
@@ -46,18 +45,41 @@ const stopPropagation = function (event) {
 }
 
 function modalLogout() {
-    const answerLogoutButtons = document.getElementById("yes-button")
-    answerLogoutButtons.addEventListener('click', () => {
+    const answerLogoutButtons = document.getElementById("yes-button-logout")
+    answerLogoutButtons.addEventListener("click", () => {
         window.sessionStorage.removeItem("token")
         window.location.href = "./index.html"
     })
 }
 
 /**
- * Fonction pour valider une opération
+ * Fonction pour effacer un projet
  */
-const validation = function validateAndDelete() {
+function modalWorkDelete(workid, modal) {
+    const answerWorkDeleteButtons = document.getElementById("yes-button-delete")
+    answerWorkDeleteButtons.addEventListener('click', async  () => {
+        try {
+            let answer = await fetch("http://localhost:" + apiPort + "/api/works/" + workid, { 
+                method: "DELETE",
+                headers: { "Authorization": "Bearer " + JSON.parse(window.sessionStorage.getItem("token"))}
+            }) 
 
+            console.log(answer)
+            if (answer.status === 401) { // Si autorisation refusée (token expiré)
+                closeModal(modal)
+                modal = document.getElementById("modal-logout-forced")
+                openModal(modal)
+                document.getElementById("ok-logout").addEventListener("click", () => window.location.href = "./login.html") 
+              }
+            if (answer.status === 404 || answer.status === 500) {
+            throw new Error('Opération impossible') 
+            }
+
+        } catch (error) {
+            console.log(error)
+  
+        }
+    })
 }
 
 /**
@@ -74,20 +96,19 @@ async function modalBackOffice() {
     showGalleryFunction(works, showGallery)
 }
 
-function showGalleryFunction(works, showGallery) {
+async function showGalleryFunction(works, showGallery) {
     // Affichage de la gallerie 
+    works = await fetch("http://localhost:" + apiPort + "/api/works").then(works => works.json())
     for (let i=0; i < works.length; i++) {
         const figureTag = document.createElement("figure")
         const imageElement = document.createElement("img")
         imageElement.src = works[i].imageUrl // ajout de l"url de l"image
         imageElement.alt = works[i].title // ajout de la balise Alt
         const trashIconLink = document.createElement("a")
-        trashIconLink.setAttribute("href", "#modal-validateDelete")
         trashIconLink.classList.add("trash")
         trashIconLink.classList.add("js-bins-modal")
         const trashIcon = document.createElement("i")
         trashIconLink.setAttribute("data-id", works[i].id)
-        trashIcon.setAttribute("data-id", works[i].id)
         trashIcon.classList.add("fa-regular")
         trashIcon.classList.add("fa-trash-can")
         trashIcon.innerHTML = ""
@@ -95,16 +116,17 @@ function showGalleryFunction(works, showGallery) {
         figureTag.appendChild(imageElement) // Affichage les nouveaux elements
         figureTag.appendChild(trashIconLink) // Affichage de l'icon trash
         showGallery.appendChild(figureTag)
-        // test des poubelles
+        
     }
-    
+    // test des poubelles
     showGallery.querySelectorAll('.js-bins-modal').forEach(a => {
         a.addEventListener('click', (event) => {
-            modal = document.querySelector(event.target.parentNode.getAttribute("href"))
-            console.log(modal)
+            modal = document.getElementById("modal-validateDelete")
             openModal(modal)
+            let deleteId = ""
+            deleteId = event.target.parentNode.dataset.id
+            modalWorkDelete(deleteId, modal)
         })
     })
     
 }
-
