19a9cf5fa951fefdd7d9377ac87cb2fc0d350cf9
Nicolas <contact@nicolasrichelet.com>
2023-10-08 19:28:29 +0200

    Ajout de ; a chaque fin de ligne

--- FrontEnd/scripts/works.js
+++ FrontEnd/scripts/works.js
@@ -4,43 +4,43 @@
  */
 function showWorks(worksFiltered) {
     // Effacer l"ancienne gallerie
-    const galleryContent = document.querySelector(".gallery")
-    galleryContent.innerHTML = ""
+    const galleryContent = document.querySelector(".gallery");
+    galleryContent.innerHTML = "";
 
     // Affichage de la gallerie 
-    for (let i=0; i < worksFiltered.length; i++) {
+    for (let i = 0; i < worksFiltered.length; i++) {
         // Reconstruction des balises
-        const figureTag = document.createElement("figure")
-        const imageElement = document.createElement("img")
-        imageElement.src = worksFiltered[i].imageUrl // ajout de l"url de l"image
-        imageElement.alt = worksFiltered[i].title // ajout de la balise Alt
-        const figcaptionTag = document.createElement("figcaption")
-        figcaptionTag.innerText = worksFiltered[i].title
-        figureTag.appendChild(imageElement) // Affichage les nouveaux elements
-        figureTag.appendChild(figcaptionTag)
-        galleryContent.appendChild(figureTag)
+        const figureTag = document.createElement("figure");
+        const imageElement = document.createElement("img");
+        imageElement.src = worksFiltered[i].imageUrl; // ajout de l"url de l"image
+        imageElement.alt = worksFiltered[i].title; // ajout de la balise Alt
+        const figcaptionTag = document.createElement("figcaption");
+        figcaptionTag.innerText = worksFiltered[i].title;
+        figureTag.appendChild(imageElement); // Affichage les nouveaux éléments
+        figureTag.appendChild(figcaptionTag);
+        galleryContent.appendChild(figureTag);
     }
 }
 
 /**
- * Cette fonction affiche les boutons de categorie
+ * Cette fonction affiche les boutons de catégorie
  */
 function showFilterButtons(categories) {
-    const filtresGallery = document.querySelector(".filtres ul")
-    const listeButton = document.createElement("li")
-    const buttonCatTous = document.createElement("button")  // Bouton catégorie "Tous"
-    buttonCatTous.dataset.id = "0"
-    buttonCatTous.innerText = "Tous"
-    buttonCatTous.classList.add("button-categorie")
-    listeButton.appendChild(buttonCatTous)
-    filtresGallery.appendChild(listeButton)
-    categories.forEach(item => {   // Autres boutons categories
-        const buttonCat = document.createElement("button")
-        buttonCat.classList.add("button-categorie")
-        buttonCat.dataset.id = item.id
-        buttonCat.innerText = item.name
-        const listeButton = document.createElement("li")
-        listeButton.appendChild(buttonCat) 
-        filtresGallery.appendChild(listeButton)
-    })
+    const filtresGallery = document.querySelector(".filtres ul");
+    const listeButton = document.createElement("li");
+    const buttonCatTous = document.createElement("button"); // Bouton catégorie "Tous"
+    buttonCatTous.dataset.id = "0";
+    buttonCatTous.innerText = "Tous";
+    buttonCatTous.classList.add("button-categorie");
+    listeButton.appendChild(buttonCatTous);
+    filtresGallery.appendChild(listeButton);
+    categories.forEach(item => { // Autres boutons catégories
+        const buttonCat = document.createElement("button");
+        buttonCat.classList.add("button-categorie");
+        buttonCat.dataset.id = item.id;
+        buttonCat.innerText = item.name;
+        const listeButton = document.createElement("li");
+        listeButton.appendChild(buttonCat); 
+        filtresGallery.appendChild(listeButton);
+    });
 }
